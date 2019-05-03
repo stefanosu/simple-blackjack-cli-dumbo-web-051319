cfbf2f4f323988531c55ddab9e8531c7c8f6980c
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-03 22:05:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -12,7 +12,7 @@ def display_card_total(card_total)
 end
 
 def prompt_user
-  puts phrase = "Type 'h' to hit or 's' to stay."
+  puts phrase = "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
